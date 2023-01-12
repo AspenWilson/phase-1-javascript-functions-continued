c160a6af468063da279f5cebd486a05bf7d747f7
Aspen Wilson <aspen.schell@gmail.com>
2023-01-12 07:23:49 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(x = "roller-skate") {
+    return `This Saturday, I want to ${x}!`
+}
+function mondayWork (x = 'go to the office') {
+    return `This Monday, I will ${x}.`
+}
+
+function wrapAdjective(x = '*') {
+    return function (adj = 'special') {
+      return `You are ${x}${adj}${x}!`;
+    };
+  }
